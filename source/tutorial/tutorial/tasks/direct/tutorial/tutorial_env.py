10d5e268879295eb878d9a3a8b15b5ff89d44a33
eureka <1806476094@qq.com>
2025-12-29 10:51:16 +0800

    更改了奖励函数

--- source/tutorial/tutorial/tasks/direct/tutorial/tutorial_env.py
+++ source/tutorial/tutorial/tasks/direct/tutorial/tutorial_env.py
@@ -555,11 +555,11 @@ def compute_rewards(
     arrived: torch.Tensor,
 ):
     total_reward = (
-        reward_velocity * 1.0
-        + 1.0
-        - penalty_smooth * 0.1
-        - collided * 20.0
-        + arrived * 5.0
+        reward_velocity * 1.0  # 速度在目标方向的分量
+        + 1.0  # 存活奖励
+        - penalty_smooth * 0.1  # 平滑度惩罚
+        - collided * 20.0  # 碰撞惩罚
+        + arrived * 200.0  # 到达奖励
     )
     return total_reward.unsqueeze(-1)  # 返回 (num_envs, 1) 通常更安全
 
