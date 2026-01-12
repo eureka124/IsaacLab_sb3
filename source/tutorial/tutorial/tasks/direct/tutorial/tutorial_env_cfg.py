c242444e6b6fcb506d6d8f7193bd261949907e41
eureka <1806476094@qq.com>
2026-01-12 20:58:35 +0800

    维度错误修正

--- source/tutorial/tutorial/tasks/direct/tutorial/tutorial_env_cfg.py
+++ source/tutorial/tutorial/tasks/direct/tutorial/tutorial_env_cfg.py
@@ -146,7 +146,7 @@ class TutorialEnvCfg(DirectRLEnvCfg):
     )  # 连续动作空间，表示机器人的线速度和角速度
     # - spaces definition
     # observation_space = 4
-    observation_space = {"camera": [3, 16, 12], "robot-state": 7, "critic-state": 15}
+    observation_space = {"camera": [3, 16, 12], "robot-state": 6, "critic-state": 15}
     state_space = 0
     # simulation
     sim: SimulationCfg = SimulationCfg(dt=1 / 120, render_interval=decimation)
