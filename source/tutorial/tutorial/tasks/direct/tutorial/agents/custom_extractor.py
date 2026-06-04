3f04fc2ec49d56b0f7c6955b6f47fa0f9ce1cb48
eureka <1806476094@qq.com>
2026-06-04 09:28:44 +0800

    注释toa地图观察空间

--- source/tutorial/tutorial/tasks/direct/tutorial/agents/custom_extractor.py
+++ source/tutorial/tutorial/tasks/direct/tutorial/agents/custom_extractor.py
@@ -230,13 +230,13 @@ class CriticFeaturesExtractor(BaseFeaturesExtractor):
             nn.LeakyReLU(),
             nn.Flatten(),
         )
-        self.toa_cnn = nn.Sequential(
-            nn.Conv2d(n_input_channels, 32, kernel_size=2, stride=1, padding=0),
-            nn.LeakyReLU(),
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=0),
-            nn.LeakyReLU(),
-            nn.Flatten(),
-        )
+        # self.toa_cnn = nn.Sequential(
+        #     nn.Conv2d(n_input_channels, 32, kernel_size=2, stride=1, padding=0),
+        #     nn.LeakyReLU(),
+        #     nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=0),
+        #     nn.LeakyReLU(),
+        #     nn.Flatten(),
+        # )
 
         # Compute CNN output dimension
         with torch.no_grad():
@@ -245,7 +245,7 @@ class CriticFeaturesExtractor(BaseFeaturesExtractor):
             sample = torch.as_tensor(camera_space.sample()[None]).float()
             cnn_output_dim = self.cnn(sample).shape[1]
             sample_toa = torch.as_tensor(observation_space["critic-toa"].sample()[None]).float()
-            toa_cnn_output_dim = self.toa_cnn(sample_toa).shape[1]
+            # toa_cnn_output_dim = self.toa_cnn(sample_toa).shape[1]
 
         # features_fc equivalent
         self.camera_fc = nn.Linear(cnn_output_dim, 192)
@@ -256,9 +256,9 @@ class CriticFeaturesExtractor(BaseFeaturesExtractor):
         self.robot_state_mlp = nn.Linear(state_dim, 192)
 
         # 2. Critic State Network (MLP)
-        critic_state_space = observation_space["critic-toa"]
+        # critic_state_space = observation_space["critic-toa"]
 
-        self.toa_mlp = nn.Linear(toa_cnn_output_dim, 192)
+        # self.toa_mlp = nn.Linear(toa_cnn_output_dim, 192)
 
         # Total features dim is the sum of both MLP outputs
         self._features_dim = 192
@@ -309,14 +309,14 @@ class CriticFeaturesExtractor(BaseFeaturesExtractor):
             obs_robot = obs_robot.unsqueeze(0)
         state_features = self.robot_state_mlp(obs_robot)
 
-        obs_critic = observations["critic-toa"]
-        if obs_critic.dim() == 3:
-            obs_critic = obs_critic.unsqueeze(0)
-        critic_features = self.toa_cnn(obs_critic)
-        critic_features = self.toa_mlp(critic_features)
+        # obs_critic = observations["critic-toa"]
+        # if obs_critic.dim() == 3:
+        #     obs_critic = obs_critic.unsqueeze(0)
+        # critic_features = self.toa_cnn(obs_critic)
+        # critic_features = self.toa_mlp(critic_features)
 
         # Sum image, robot-state and critic features element-wise
-        return img_features + state_features + critic_features
+        return img_features + state_features  # + critic_features
 
 
 class GodViewExtractor(BaseFeaturesExtractor):
